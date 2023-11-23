137ba3e8650463fd613d2ee21bf9ced16f52b478
zakirya592 <muhammadzakirya592@gmail.com>
2023-11-23 18:49:14 +0500

    updata

--- assets/Screen/Work Request/Createworkrequest.jsx
+++ assets/Screen/Work Request/Createworkrequest.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from "react-native"
-import { Picker } from '@react-native-picker/picker';
+import { Dropdown } from 'react-native-element-dropdown';
 
+const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+];
 export default function Createworkrequest() {
-    const [selectedValue, setSelectedValue] = useState('');
-    const [WorkRequest, setWorkRequest] = useState('')
-     const [isFocused, setIsFocused] = useState(false);
+    const [value, setvalue] = useState({
+        Employeeid: null, WorkRequest:''
+    })
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFocus, setIsFocus] = useState(false);
 
     return (
         <View>
@@ -18,18 +30,31 @@ export default function Createworkrequest() {
                 <View style={styles.singleinputlable}>
                     <Text style={styles.lableinput}>Employee ID
                     </Text>
-                    <Picker
-                        selectedValue={selectedValue}
-                        style={[styles.inputBox, styles.dropdown]}
-                        itemStyle={{ height: 10 }} // Set height for the items in the Picker
-                        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                    >
-                        <Picker.Item style={{height:12}} label="Option 1" value="option1" />
-                        <Picker.Item style={{ height: 12 }} label="Option 2" value="option2" />
-                        <Picker.Item style={{ height: 12 }} label="Option 3" value="option3" />
-                        {/* Add more Picker.Item components for additional options */}
-                    </Picker>
-
+                    <Dropdown
+                        style={[styles.inputBox,{height:40,}, isFocus && { borderColor: 'blue' }]}
+                        placeholderStyle={styles.placeholderStyle}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        inputSearchStyle={styles.inputSearchStyle}
+                        iconStyle={styles.iconStyle}
+                        data={data}
+                        search
+                        maxHeight={200}
+                        labelField="label"
+                        valueField="value"
+                        placeholder={!isFocus ? 'Select item' : '...'}
+                        searchPlaceholder="Search..."
+                        value={value.Employeeid}
+                        onFocus={() => setIsFocus(true)}
+                        onBlur={() => setIsFocus(false)}
+                        onChange={item => {
+                            setvalue((prevValue) => ({
+                                ...prevValue,
+                                Employeeid: item.value, // Update the Employeeid property
+                            }));
+                            setIsFocus(false);
+                        }}
+                       
+                    />
                 </View>
 
                 <View style={styles.singleinputlable}>
@@ -38,14 +63,18 @@ export default function Createworkrequest() {
                     <TextInput
                         style={[
                             styles.inputBox,
-                            { borderColor: isFocused ? '#1D3A9F' : '#94A0CA' }, // Dynamic border color
+                            { borderColor: isFocused ? '#1D3A9F' : '#94A0CA' },
                         ]}
-                        // value={WorkRequest}
-                        // onChangeText={(text) => WorkRequest(text)}
+                        value={value.WorkRequest}
+                        onChange={item => {
+                            setvalue((prevValue) => ({
+                                ...prevValue,
+                                WorkRequest: item.value, // Update the Employeeid property
+                            }));
+                        }}
                         placeholder="Work Request #"
                         placeholderTextColor="#94A0CA"
                         selectionColor="#fff"
-                        // keyboardType="Work Request #"
                         underlineColorAndroid="transparent"
                         onFocus={(() => {
                             setIsFocused(true);
@@ -55,12 +84,30 @@ export default function Createworkrequest() {
                         })}
                     />
                 </View>
-                   
-                    </View>
+
+            </View>
+            
+            
         </View>
     )
 }
 const styles = StyleSheet.create({
+
+    placeholderStyle: {
+        fontSize: 16,
+        color:'#94A0CA'
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 30,
+        fontSize: 16,
+    },
     prograp: {
         color: '#1E3B8B',
         fontSize: 16,
@@ -69,25 +116,20 @@ const styles = StyleSheet.create({
         margin: 5
     },
     inputContainer: {
+        marginTop:20,
         flexDirection: 'row',
         alignItems: 'center',
         paddingBottom: 5,
         marginBottom: 10,
         position: 'relative',
-        justifyContent:'space-around'
+        justifyContent: 'space-around'
     },
-    lableinput:{
+    lableinput: {
         color: '#0A2DAA',
-        fontSize:16,
-        fontWeight:'400'
+        fontSize: 16,
+        fontWeight: '400'
     },
-    picker: {
-        width: 150, // Set the width directly
-        height: 50, // Set the height directly
-        borderWidth: 1,
-        borderColor: 'red', // Set the border color to red
-    },
-     inputBox: {
+    inputBox: {
         width: 160,
         borderRadius: 5,
         paddingHorizontal: 10,
@@ -96,8 +138,8 @@ const styles = StyleSheet.create({
         fontSize: 15,
         color: 'black',
         marginVertical: 9,
-        paddingVertical:5,
-        backgroundColor:'#FFFFFF',
+        paddingVertical: 5,
+        backgroundColor: '#FFFFFF',
     },
-  
+
 })
